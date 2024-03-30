55a88a7a9ba71a94c4bd97c9a841038aad3e278e
FranGuam <frankguansy@icloud.com>
2024-03-31 00:46:39 +0800

    feat(exp2): update script

--- exp2/benchmark.sh
+++ exp2/benchmark.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 set -u
-set -x
 
 source /home/spack/spack/share/spack/setup-env.sh
 
 spack load openmpi
 
+set -x
+
 for node in 1 2 4; do
     for process in 4 8 16 32 64; do
-        for scale in 1000000 10000000 100000000 1000000000 10000000000; do
+        for scale in 64 1024 16384 262144 4194304 67108864 1073741824; do
             srun -N $node -n $process ./allreduce 10 $scale
         done
     done
