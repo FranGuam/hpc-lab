7fc3e8d55ca7cfe54fcf16a17f9a24ce1192834d
FranGuam <frankguansy@hotmail.com>
2024-04-06 14:51:51 +0800

    chore(PA1): debug

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -159,10 +159,12 @@ void Worker::sort() {
     }
     finished = upward_count == 0 && downward_count == 0 && slice_num == 1;
     if (!finished && downward_count < slice_num / 2 && upward_count < slice_num / 2) {
+      if (rank == 2) std::cout << "Entered" << std::endl;
       data_buf = new float[block_len];
       #pragma omp parallel for schedule(guided)
       for (int i = 0; i < slice_num; i += 2) {
         if (i == slice_num - 1) {
+          if (rank == 2) std::cout << "Entered 1" << std::endl;
           std::merge(
             data + (i - 2) * current_sort_size, data + i * current_sort_size,
             data + i * current_sort_size, data + block_len,
@@ -171,6 +173,7 @@ void Worker::sort() {
           std::copy(data_buf + (i - 2) * current_sort_size, data_buf + block_len, data + (i - 2) * current_sort_size);
         }
         else if (i == slice_num - 2) {
+          if (rank == 2) std::cout << "Entered 2" << std::endl;
           std::merge(
             data + i * current_sort_size, data + (i + 1) * current_sort_size,
             data + (i + 1) * current_sort_size, data + block_len,
@@ -179,6 +182,7 @@ void Worker::sort() {
           std::copy(data_buf + i * current_sort_size, data_buf + block_len, data + i * current_sort_size);
         }
         else {
+          if (rank == 2) std::cout << "Entered 3" << std::endl;
           std::merge(
             data + i * current_sort_size, data + (i + 1) * current_sort_size,
             data + (i + 1) * current_sort_size, data + (i + 2) * current_sort_size,
