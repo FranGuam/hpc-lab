20f52a6bd458482a96f7ad2d0537b80c01d74e1a
FranGuam <frankguansy@icloud.com>
2024-04-15 02:03:20 +0800

    fix(PA1)

--- PA1/test.sh
+++ PA1/test.sh
@@ -6,13 +6,15 @@ source /home/spack/spack/share/spack/setup-env.sh
 
 spack load openmpi
 
-set -x
+make -j4
 
 echo "Testing Provided Data"
 for k in 100 1000 10000 100000 1000000 10000000 100000000; do
     for i in 1 2; do
         for j in {1..28}; do
+            echo "Testing $k data with $i node and $j process"
             srun -N $i -n $j ./odd_even_sort $k data/$k.dat
+            echo "\n"
         done
     done
 done
