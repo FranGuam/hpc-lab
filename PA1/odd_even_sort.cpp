5d71c9194b7999403b801c883b61821876e07ca8
FranGuam <frankguansy@icloud.com>
2024-04-14 17:40:59 +0800

    fix(PA1)

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <mpi.h>
+#include <string.h>
 
 #include "worker.h"
 
@@ -11,24 +12,59 @@ void radixSort(float* data, int n) {
   int* offset = new int[256];
   int* temp = new int[n];
   int* data_int = (int*)data;
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 256; j++) {
-      count[j] = 0;
-    }
-    for (int j = 0; j < n; j++) {
-      count[(data_int[j] >> (i * 8)) & 0xff]++;
-    }
-    offset[0] = 0;
-    for (int j = 1; j < 256; j++) {
-      offset[j] = offset[j - 1] + count[j - 1];
-    }
-    for (int j = 0; j < n; j++) {
-      temp[offset[(data_int[j] >> (i * 8)) & 0xff]++] = data_int[j];
-    }
-    for (int j = 0; j < n; j++) {
-      data_int[j] = temp[j];
-    }
+
+  // 0-7
+  memset(count, 0, sizeof(int) * 256);
+  for (int j = 0; j < n; j++) {
+    count[data_int[j]]++;
   }
+  offset[0] = 0;
+  for (int j = 1; j < 256; j++) {
+    offset[j] = offset[j - 1] + count[j - 1];
+  }
+  for (int j = 0; j < n; j++) {
+    temp[offset[data_int[j]]++] = data_int[j];
+  }
+
+  // 8-15
+  memset(count, 0, sizeof(int) * 256);
+  for (int j = 0; j < n; j++) {
+    count[temp[j] >> 8]++;
+  }
+  offset[0] = 0;
+  for (int j = 1; j < 256; j++) {
+    offset[j] = offset[j - 1] + count[j - 1];
+  }
+  for (int j = 0; j < n; j++) {
+    data_int[offset[temp[j] >> 8]++] = temp[j];
+  }
+
+  // 16-23
+  memset(count, 0, sizeof(int) * 256);
+  for (int j = 0; j < n; j++) {
+    count[data_int[j] >> 16]++;
+  }
+  offset[0] = 0;
+  for (int j = 1; j < 256; j++) {
+    offset[j] = offset[j - 1] + count[j - 1];
+  }
+  for (int j = 0; j < n; j++) {
+    temp[offset[data_int[j] >> 16]++] = data_int[j];
+  }
+
+  // 24-31
+  memset(count, 0, sizeof(int) * 256);
+  for (int j = 0; j < n; j++) {
+    count[temp[j] >> 24]++;
+  }
+  offset[0] = 0;
+  for (int j = 1; j < 256; j++) {
+    offset[j] = offset[j - 1] + count[j - 1];
+  }
+  for (int j = 0; j < n; j++) {
+    data_int[offset[temp[j] >> 24]++] = temp[j];
+  }
+
   delete[] count;
   delete[] offset;
   delete[] temp;
