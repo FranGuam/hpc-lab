98f64172c57b7e86f3902a17d48b894ee91159fc
FranGuam <frankguansy@icloud.com>
2024-04-15 02:50:16 +0800

    fix(PA1)

--- PA1/test.sh
+++ PA1/test.sh
@@ -9,12 +9,15 @@ spack load openmpi
 make -j4
 
 echo "Testing Provided Data"
-for k in 100 1000 10000 100000 1000000 10000000 100000000; do
-    for i in 1 2; do
-        for j in {1..28}; do
-            echo "Testing $k data with $i node and $j process"
-            srun -N $i -n $j ./odd_even_sort $k data/$k.dat
-            echo " "
-        done
+for i in 100 1000 10000 100000 1000000 10000000 100000000; do
+    for j in {1..28}; do
+        echo "Testing $i data with 1 node and $j process"
+        srun -N 1 -n $j ./odd_even_sort $i data/$i.dat
+        echo " "
+    done
+    for j in {29..56}; do
+        echo "Testing $i data with 2 node and $j process"
+        srun -N 2 -n $j ./odd_even_sort $i data/$i.dat
+        echo " "
     done
 done
