eed82304ee3b3e5f96b25904ba38404b1f910a9c
FranGuam <frankguansy@icloud.com>
2024-04-14 19:42:32 +0800

    fix(PA1)

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -102,7 +102,7 @@ void Worker::sort() {
   float* send_buf = new float[(block_size + block_len + 1) / 2];
   MPI_Request request;
 
-  for (int i = 0; i < nprocs + 1; i++) {
+  for (int i = 0; i < nprocs * 2; i++) {
     if (i) MPI_Wait(&request, nullptr);
     if (!last_rank) {
       MPI_Isend(data, block_len / 2, MPI_FLOAT, rank + 1, rank, MPI_COMM_WORLD, &request);
