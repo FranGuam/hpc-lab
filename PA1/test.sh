9792a09ae6a21e76ab532a6ab16eeed7a61e742c
FranGuam <frankguansy@icloud.com>
2024-04-22 00:46:38 +0800

    fix(PA1)

--- PA1/test.sh
+++ PA1/test.sh
@@ -21,9 +21,41 @@ make -j4
 #     done
 # done
 
+echo "Test on Random Data"
+for i in 31 1000 23459 8972407 112000000; do
+    ./generate $i ./$i.dat random
+    for j in {1..28}; do
+        echo "Testing $i data with 1 node and $j process"
+        srun -N 1 -n $j ./odd_even_sort $i ./$i.dat
+        echo " "
+    done
+    for j in {29..56}; do
+        echo "Testing $i data with 2 node and $j process"
+        srun -N 2 -n $j ./odd_even_sort $i data/$i.desc
+        echo " "
+    done
+    rm ./$i.dat
+done
+
 echo "Test on Descending Data"
 for i in 31 1000 23459 8972407 112000000; do
-    ./generate $i ./$i.dat
+    ./generate $i ./$i.dat descending
+    for j in {1..28}; do
+        echo "Testing $i data with 1 node and $j process"
+        srun -N 1 -n $j ./odd_even_sort $i ./$i.dat
+        echo " "
+    done
+    for j in {29..56}; do
+        echo "Testing $i data with 2 node and $j process"
+        srun -N 2 -n $j ./odd_even_sort $i data/$i.desc
+        echo " "
+    done
+    rm ./$i.dat
+done
+
+echo "Test on Identical Data"
+for i in 31 1000 23459 8972407 112000000; do
+    ./generate $i ./$i.dat identical
     for j in {1..28}; do
         echo "Testing $i data with 1 node and $j process"
         srun -N 1 -n $j ./odd_even_sort $i ./$i.dat
