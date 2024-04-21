ad33a05e43395380c9b16da7fc84adba53d9e078
FranGuam <frankguansy@icloud.com>
2024-04-21 18:09:17 +0800

    feat(exp5): complete code

--- exp5/aplusb-intrinsic.cpp
+++ exp5/aplusb-intrinsic.cpp
@@ -3,4 +3,10 @@
 
 void a_plus_b_intrinsic(float* a, float* b, float* c, int n) {
     // Your code here
+    for (int i = 0; i < n; i += 8) {
+        __m256 a_vec = _mm256_load_ps(a + i);
+        __m256 b_vec = _mm256_load_ps(b + i);
+        __m256 c_vec = _mm256_add_ps(a_vec, b_vec);
+        _mm256_store_ps(c + i, c_vec);
+    }
 }
