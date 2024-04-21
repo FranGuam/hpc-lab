ea1ca7f122436f0c116a0fdfb6d0a2eab8fc5adc
FranGuam <frankguansy@icloud.com>
2024-04-22 01:53:08 +0800

    fix(PA1)

--- PA1/run.sh
+++ PA1/run.sh
@@ -6,8 +6,6 @@ if [ $# != 3 ]; then
     exit 1
 fi
 
-declare -i process
-declare -i data_size
 executable=$1
 data_size=$2
 data_file=$3
@@ -20,20 +18,20 @@ if [ ! -f $data_file ]; then
     echo "Data file $data_file not found!"
     exit 1
 fi
-if [ data_size -lt 0 ]; then
+if [ $data_size -lt 0 ]; then
     echo "Data size must be positive!"
     exit 1
 fi
-if [ data_size -gt 2147483647 ]; then
+if [ $data_size -gt 2147483647 ]; then
     echo "Data size must be less than 100000000!"
     exit 1
 fi
 
-if [ data_size -lt 10000 ]; then
+if [ $data_size -lt 10000 ]; then
     process=1
 else
     process=56
-    while [ process -gt 6 ]; do
+    while [ $process -gt 6 ]; do
         block_size=$(((data_size + process - 1) / process))
         last_block_len=$((data_size % block_size))
         if [ block_size % 2 -eq 0 -a last_block_len * 2 -ge block_size]; then
@@ -41,7 +39,7 @@ else
         fi
         process=$((process - 1))
     done
-    if [ process -eq 6 ]; then
+    if [ $process -eq 6 ]; then
         process=56
         while [ process -gt 6 ]; do
             block_size=$(((data_size + process - 1) / process))
@@ -51,12 +49,12 @@ else
             process=$((process - 1))
         done
     fi
-    if [ process -eq 6 ]; then
+    if [ $process -eq 6 ]; then
         process=28
     fi
 fi
 node=1
-if [ process -gt 28 ]; then
+if [ $process -gt 28 ]; then
     node=2
 fi
 
