647b1917fefd5ca5f3798ab31888ea393983c6c5
FranGuam <frankguansy@icloud.com>
2024-04-22 15:21:41 +0800

    feat(PA1): prepare for benchmark

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -28,21 +28,41 @@ int main(int argc, char **argv) {
   }
 
   Worker *worker = new Worker(n, nprocs, rank);
-  /** Read input data from the input file */
-  worker->input(input_name);
+  unsigned long *time = new unsigned long[10];
 
-  /** Sort the list (input data) */
-  timeval start, end;
-  unsigned long time;
-  MPI_Barrier(MPI_COMM_WORLD);
-  gettimeofday(&start, NULL);
+  for (int i = 0; i < 5; i++)
+  {
+    /** Read input data from the input file */
+    worker->input(input_name);
+    /** Sort the list (input data) */
+    MPI_Barrier(MPI_COMM_WORLD);
+    // run your code
+    worker->sort();
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    /** Read input data from the input file */
+    worker->input(input_name);
+    /** Sort the list (input data) */
+    timeval start, end;
+    MPI_Barrier(MPI_COMM_WORLD);
+    gettimeofday(&start, NULL);
+    // run your code
+    worker->sort();
+    MPI_Barrier(MPI_COMM_WORLD);
+    gettimeofday(&end, NULL);
+    time[i] = 1000000.0 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+  }
 
-  // run your code
-  worker->sort();
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  gettimeofday(&end, NULL);
-  time = 1000000.0 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+  unsigned long avg = 0, min = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    avg += time[i];
+    if (time[i] < min || min == 0)
+      min = time[i];
+  }
+  avg /= 10.0;
 
   /** Check the sorted list */
   int ret = worker->check();
@@ -55,7 +75,8 @@ int main(int argc, char **argv) {
   }
   MPI_Barrier(MPI_COMM_WORLD);
   if (rank == 0) {
-    printf("Execution time of function sort is %lf ms.\n", time / 1000.0);
+    printf("Execution time of function sort is %lf ms.\n", avg / 1000.0);
+    printf("Minimum execution time of function sort is %lf ms.\n", min / 1000.0);
   }
 
 #ifndef NDEBUG
