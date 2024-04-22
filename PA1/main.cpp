260557d547118a5d42d5a5f8425a53bc6b50341b
FranGuam <frankguansy@icloud.com>
2024-04-22 15:48:01 +0800

    fix(PA1)

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
   }
 
   Worker *worker = new Worker(n, nprocs, rank);
-  unsigned long *time = new unsigned long[10];
+  unsigned long *time = new unsigned long[20];
 
   for (int i = 0; i < 5; i++)
   {
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
     worker->sort();
     MPI_Barrier(MPI_COMM_WORLD);
   }
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 20; i++)
   {
     /** Read input data from the input file */
     worker->input(input_name);
@@ -55,14 +55,9 @@ int main(int argc, char **argv) {
     time[i] = 1000000.0 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
   }
 
-  unsigned long avg = 0, min = 0;
-  for (int i = 0; i < 10; i++)
-  {
-    avg += time[i];
-    if (time[i] < min || min == 0)
-      min = time[i];
-  }
-  avg /= 10.0;
+  std::sort(time, time + 20);
+  unsigned long med = time[10];
+  unsigned long min = time[0];
 
   /** Check the sorted list */
   int ret = worker->check();
@@ -75,7 +70,7 @@ int main(int argc, char **argv) {
   }
   MPI_Barrier(MPI_COMM_WORLD);
   if (rank == 0) {
-    printf("Execution time of function sort is %lf ms.\n", avg / 1000.0);
+    printf("Execution time of function sort is %lf ms.\n", med / 1000.0);
     printf("Minimum execution time of function sort is %lf ms.\n", min / 1000.0);
   }
 
