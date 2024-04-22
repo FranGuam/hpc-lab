54657bf1b90278b7f87d1b2e126b1471c9baee0e
FranGuam <frankguansy@icloud.com>
2024-04-22 22:05:29 +0800

    feat(PA1): bind cores

--- PA1/run.sh
+++ PA1/run.sh
@@ -27,32 +27,31 @@ if [ $data_size -gt 2147483647 ]; then
     exit 1
 fi
 
-if [ $data_size -lt 10000 ]; then
+if [ $data_size -lt 14000 ]; then
     process=1
 else
-    process=1
-    # process=56
-    # while [ $process -gt 6 ]; do
-    #     block_size=$(((data_size + process - 1) / process))
-    #     last_block_len=$((data_size % block_size))
-    #     if [ $(($block_size % 2)) -eq 0 -a $(($last_block_len * 2)) -ge $block_size ]; then
-    #         break
-    #     fi
-    #     process=$((process - 1))
-    # done
-    # if [ $process -eq 6 ]; then
-    #     process=56
-    #     while [ $process -gt 6 ]; do
-    #         block_size=$(((data_size + process - 1) / process))
-    #         if [ $(($block_size % 2)) -eq 0 ]; then
-    #             break
-    #         fi
-    #         process=$((process - 1))
-    #     done
-    # fi
-    # if [ $process -eq 6 ]; then
-    #     process=28
-    # fi
+    process=56
+    while [ $process -gt 6 ]; do
+        block_size=$(((data_size + process - 1) / process))
+        last_block_len=$((data_size % block_size))
+        if [ $(($block_size % 2)) -eq 0 -a $(($last_block_len * 2)) -ge $block_size ]; then
+            break
+        fi
+        process=$((process - 1))
+    done
+    if [ $process -eq 6 ]; then
+        process=56
+        while [ $process -gt 6 ]; do
+            block_size=$(((data_size + process - 1) / process))
+            if [ $(($block_size % 2)) -eq 0 ]; then
+                break
+            fi
+            process=$((process - 1))
+        done
+    fi
+    if [ $process -eq 6 ]; then
+        process=28
+    fi
 fi
 node=1
 if [ $process -gt 28 ]; then
@@ -60,4 +59,4 @@ if [ $process -gt 28 ]; then
 fi
 
 echo "Running $data_size data with $process process"
-srun -N $node -n $process $executable $data_size $data_file
+srun -N $node -n $process --cpu-bind=cores $executable $data_size $data_file
