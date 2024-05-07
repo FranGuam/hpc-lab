b5e69545c97dbbe01a2a173f0b87e7d8257d24f0
FranGuam <frankguansy@icloud.com>
2024-05-07 16:43:20 +0800

    feat(PA2): script

---
+++ PA2/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+source /home/spack/spack/share/spack/setup-env.sh
+
+spack load cuda && spack load gcc@10.2.0
+
+make
+
+srun -N 1 --gres=gpu:1 ./benchmark 100
+srun -N 1 --gres=gpu:1 ./benchmark 1000
+srun -N 1 --gres=gpu:1 ./benchmark 2500
+srun -N 1 --gres=gpu:1 ./benchmark 5000
+srun -N 1 --gres=gpu:1 ./benchmark 7500
+srun -N 1 --gres=gpu:1 ./benchmark 10000
+
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off ./benchmark 100
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics achieved_occupancy ./benchmark 100
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics gld_throughput ./benchmark 100
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics gld_efficiency ./benchmark 100
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics ipc ./benchmark 100
+
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off ./benchmark 1000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics achieved_occupancy ./benchmark 1000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics gld_throughput ./benchmark 1000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics gld_efficiency ./benchmark 1000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics ipc ./benchmark 1000
+
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off ./benchmark 10000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics achieved_occupancy ./benchmark 10000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics gld_throughput ./benchmark 10000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics gld_efficiency ./benchmark 10000
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics ipc ./benchmark 10000
