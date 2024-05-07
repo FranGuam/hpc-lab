bc589fa03f96960ec4c6ccafcbd54f0606775308
FranGuam <frankguansy@icloud.com>
2024-05-07 20:55:13 +0800

    feat(PA2): reduce shared memory use

--- PA2/apsp.cu
+++ PA2/apsp.cu
@@ -53,23 +53,23 @@ __global__ void stage2(int n, int p, int *graph) {
 }
 
 __global__ void stage3(int n, int p, int *graph) {
-    __shared__ int shared[3 * BLOCK_SIZE * BLOCK_SIZE];
+    __shared__ int shared[2 * BLOCK_SIZE * BLOCK_SIZE];
     auto i = (blockIdx.y < p ? blockIdx.y : blockIdx.y + 1) * blockDim.y + threadIdx.y;
     auto j = (blockIdx.x < p ? blockIdx.x : blockIdx.x + 1) * blockDim.x + threadIdx.x;
     auto ii = p * blockDim.y + threadIdx.y;
     auto jj = p * blockDim.x + threadIdx.x;
     auto blockSize = blockDim.x * blockDim.y;
-    if (i < n && j < n) shared(threadIdx.y, threadIdx.x) = graph(i, j);
+    if (i < n && jj < n) shared(threadIdx.y, threadIdx.x) = graph(i, jj);
     else shared(threadIdx.y, threadIdx.x) = DATA_RANGE;
-    if (i < n && jj < n) shared_offset(threadIdx.y, threadIdx.x, blockSize) = graph(i, jj);
+    if (ii < n && j < n) shared_offset(threadIdx.y, threadIdx.x, blockSize) = graph(ii, j);
     else shared_offset(threadIdx.y, threadIdx.x, blockSize) = DATA_RANGE;
-    if (ii < n && j < n) shared_offset(threadIdx.y, threadIdx.x, blockSize * 2) = graph(ii, j);
-    else shared_offset(threadIdx.y, threadIdx.x, blockSize * 2) = DATA_RANGE;
     __syncthreads();
-    int tmp = shared(threadIdx.y, threadIdx.x);
+    int tmp;
+    if (i < n && j < n) tmp = graph(i, j);
+    else tmp = DATA_RANGE;
     # pragma unroll 32
     for (int k = 0; k < BLOCK_SIZE; k++) {
-        tmp = min(tmp, shared_offset(threadIdx.y, k, blockSize) + shared_offset(k, threadIdx.x, blockSize * 2));
+        tmp = min(tmp, shared(threadIdx.y, k) + shared_offset(k, threadIdx.x, blockSize));
     }
     if (i < n && j < n) graph(i, j) = tmp;
 }
