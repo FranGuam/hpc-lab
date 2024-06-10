88e6285cb73ba2c95c3839e8ad7df2d5340cdf94
FranGuam <frankguansy@icloud.com>
2024-06-11 00:15:45 +0800

    refactor(PA3)

--- PA3/include/spmm_opt.h
+++ PA3/include/spmm_opt.h
@@ -8,8 +8,6 @@ public:
     SpMMOpt(int *dev_out_ptr, int *dev_out_idx, int out_num_v, int out_num_e, int out_feat_in) : SpMM(dev_out_ptr, dev_out_idx, out_num_v, out_num_e, out_feat_in) {}
     SpMMOpt(CSR *g, int out_feat_in) : SpMM(g, out_feat_in) {}
     ~SpMMOpt() {
-        if (target) checkCudaErrors(cudaFree(target));
-        if (ptr_scheduled) checkCudaErrors(cudaFree(ptr_scheduled));
         if (d_coo) checkCudaErrors(cudaFree(d_coo));
     }
      
@@ -17,14 +15,7 @@ public:
 
     virtual void run(float *vin, float *vout);
 
-    void edgesort();
-
-    void neighbor_grouping(int neighbor_num);
-
 private:
-    int num_target;
-    int *target, *ptr_scheduled;
-
     int *d_coo;
 };
 #endif
