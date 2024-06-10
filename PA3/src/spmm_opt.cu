4cde65c61b1a996f69e7e203ce9a07b35ecd390e
FranGuam <frankguansy@icloud.com>
2024-06-10 21:58:45 +0800

    feat(PA3): warp load together

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -14,14 +14,19 @@ __global__ void csr2coo_kernel(int *ptr, int *coo, int num_v)
 
 __global__ void spmm_kernel_opt(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int INFEATURE, int *coo, int num_e)
 {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int tid = blockIdx.x * blockDim.x + threadIdx.y;
     if (tid >= num_e) return;
     int row = coo[tid];
     int col = idx[tid];
     float value = val[tid];
-    for (int j = 0; j < INFEATURE; ++j)
-    {
-        atomicAdd(&vout[row * INFEATURE + j], vin[col * INFEATURE + j] * value);
+    if (INFEATURE == 32) {
+        atomicAdd(&vout[row * INFEATURE + threadIdx.x], vin[col * INFEATURE + threadIdx.x] * value);
+    }
+    else {
+        for (int j = 0; j < 8; ++j)
+        {
+            atomicAdd(&vout[row * INFEATURE + j * 32 + threadIdx.x], vin[col * INFEATURE + j * 32 + threadIdx.x] * value);
+        }
     }
 }
 
@@ -31,9 +36,10 @@ void SpMMOpt::preprocess(float *vin, float *vout)
     checkCudaErrors(cudaMalloc2((void**)&d_coo, num_e * sizeof(int)));
     csr2coo_kernel<<<(num_e + 127) / 128, 128>>>(d_ptr, d_coo, num_v);
 
-    int BLOCK_SIZE = 128;
+    int BLOCK_SIZE = 32;
     grid.x = (num_e + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    block.x = BLOCK_SIZE;
+    block.y = BLOCK_SIZE;
+    block.x = 32;
 }
 
 void SpMMOpt::run(float *vin, float *vout)
