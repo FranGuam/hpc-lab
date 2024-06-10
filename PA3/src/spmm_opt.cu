feb7f2e74bde9c6db64397ec9de29bf1a8c89d76
FranGuam <frankguansy@icloud.com>
2024-06-11 00:41:40 +0800

    refactor(PA3)

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -1,6 +1,10 @@
 #include "spmm_opt.h"
 #include "util.h"
 
+#define SMALL_WIDTH 32
+#define LARGE_WIDTH 256
+#define BLOCK_LEN 32
+
 __global__ void csr2coo_kernel(int *ptr, int *coo, int num_v)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -21,21 +25,22 @@ __global__ void column_major_kernel(int *coo, int *idx, float *val, int *col_idx
     value[tid] = val[tid];
 }
 
-__global__ void spmm_kernel_opt(int *col_idx, int *row_idx, float *value, float *vin, float *vout, int num_v, int num_e, int INFEATURE)
+__global__ void spmm_kernel_opt(int *col_idx, int *row_idx, float *value, float *vin, float *vout, int num_v, int num_e, int feat_in)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.y;
     if (tid >= num_e) return;
     int row = row_idx[tid];
     int col = col_idx[tid];
     float val = value[tid];
-    int src_idx = col * INFEATURE + threadIdx.x;
-    int dst_idx = row * INFEATURE + threadIdx.x;
-    if (INFEATURE == 32) {
+    int src_idx = col * feat_in + threadIdx.x;
+    int dst_idx = row * feat_in + threadIdx.x;
+    if (feat_in == SMALL_WIDTH) {
         atomicAdd(&vout[dst_idx], vin[src_idx] * val);
     }
     else 
     {
-        for (int j = 0; j < 256; j += 32)
+        #pragma unroll 8
+        for (int j = 0; j < LARGE_WIDTH; j += BLOCK_LEN)
         {
             atomicAdd(&vout[dst_idx + j], vin[src_idx + j] * val);
         }
@@ -60,10 +65,10 @@ void SpMMOpt::preprocess(float *vin, float *vout)
     column_major_kernel<<<num_blocks, block_size>>>(d_coo, d_idx, d_val, d_col_idx, d_row_idx, d_value, num_e);
 
     // Decide grid and block size for spmm_kernel_opt
-    int BLOCK_SIZE = 32;
-    grid.x = (num_e + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    block.y = BLOCK_SIZE;
-    block.x = 32;
+    block_size = 32;
+    grid.x = (num_e + block_size - 1) / block_size;
+    block.y = block_size;
+    block.x = BLOCK_LEN;
 }
 
 void SpMMOpt::run(float *vin, float *vout)
