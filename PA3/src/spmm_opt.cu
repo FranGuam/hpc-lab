1cf7062038d82b08ce26a92867398837c7cbee29
FranGuam <frankguansy@icloud.com>
2024-06-27 23:27:10 +0800

    feat(PA3): using Ge-spMM

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -1,84 +1,69 @@
 #include "spmm_opt.h"
-#include "util.h"
 
-#define SMALL_WIDTH 32
-#define LARGE_WIDTH 256
-#define BLOCK_LEN 32
+__global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v)
+{
+    __shared__ int s_idx[32];
+    __shared__ float s_val[32];
+    int tid = blockIdx.x * blockDim.y + threadIdx.y;
+    if (tid >= num_v) return;
+    int begin = ptr[tid], end = ptr[tid + 1];
+    int tmp = 0;
+    for (int i = begin; i < end; i += 32)
+    {
+        // Load data into shared memory
+        int ii = i + threadIdx.x;
+        if (ii < end)
+        {
+            s_idx[threadIdx.x] = idx[ii];
+            s_val[threadIdx.x] = val[ii];
+        }
+        __syncwarp();
 
-__global__ void csr2coo_kernel(int *ptr, int *coo, int num_v)
+        // Compute
+        for (int j = 0; j < min(32, end - i); ++j)
+        {
+            tmp += vin[s_idx[j] * 32 + threadIdx.x] * s_val[j];
+        }
+        __syncwrap();
+    }
+    vout[tid * 32 + threadIdx.x] = tmp;
+}
+
+__global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid >= num_v) return;
     int begin = ptr[tid], end = ptr[tid + 1];
-    for (int i = begin; i < end; ++i)
+    for (int j = 0; j < 256; ++j)
     {
-        coo[i] = tid;
+        float result = 0.0f;
+        for (int i = begin; i < end; ++i)
+        {
+            result += vin[idx[i] * 256 + j] * val[i];
+        }
+        vout[tid * 256 + j] = result;
     }
 }
 
-__global__ void transform_values_kernel(int *perm, float *val, float *new_val, int num_e)
+void SpMMRef::preprocess(float *vin, float *vout)
 {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid >= num_e) return;
-    new_val[tid] = val[perm[tid]];
+
 }
 
-__global__ void spmm_kernel_opt(int *col_idx, int *row_idx, float *value, float *vin, float *vout, int num_v, int num_e, int feat_in)
+void SpMMRef::run(float *vin, float *vout)
 {
-    int tid = blockIdx.x * blockDim.y + threadIdx.y;
-    if (tid >= num_e) return;
-    int row = row_idx[tid];
-    int col = col_idx[tid];
-    float val = value[tid];
-    int src_idx = col * feat_in + threadIdx.x;
-    int dst_idx = row * feat_in + threadIdx.x;
-    for (int j = 0; j < feat_in; j += blockDim.x)
+    if (feat_in == 32)
     {
-        atomicAdd(&vout[dst_idx + j], vin[src_idx + j] * val);
+        block.y = 32;
+        grid.x = (num_v + block.y - 1) / block.y;
+        block.x = 32;
+        spmm_kernel_opt32<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v);
     }
-}
-
-void SpMMOpt::preprocess(float *vin, float *vout)
-{
-    int block_size, num_blocks;
-    // Convert CSR to COO
-    block_size = 128;
-    num_blocks = (num_v + block_size - 1) / block_size;
-    checkCudaErrors(cudaMalloc2((void**)&d_coo, num_e * sizeof(int)));
-    csr2coo_kernel<<<num_blocks, block_size>>>(d_ptr, d_coo, num_v);
-
-    // Copy Data for transformation
-    checkCudaErrors(cudaMalloc2((void**)&d_col_idx, num_e * sizeof(int)));
-    checkCudaErrors(cudaMalloc2((void**)&d_row_idx, num_e * sizeof(int)));
-    checkCudaErrors(cudaMalloc2((void**)&d_value, num_e * sizeof(float)));
-    checkCudaErrors(cudaMemcpy(d_row_idx, d_coo, num_e * sizeof(int), cudaMemcpyDeviceToDevice));
-    checkCudaErrors(cudaMemcpy(d_col_idx, d_idx, num_e * sizeof(int), cudaMemcpyDeviceToDevice));
-    checkCudaErrors(cudaMemcpy(d_value, d_val, num_e * sizeof(float), cudaMemcpyDeviceToDevice));
-
-    // Convert Row-major to Column-major
-    void *d_buffer;
-    size_t buffer_size;
-    int *d_perm;
-    checkCudaErrors(cusparseCreate(&handle));
-    checkCudaErrors(cusparseXcoosort_bufferSizeExt(handle, num_v, num_v, num_e, d_row_idx, d_col_idx, &buffer_size));
-    checkCudaErrors(cudaMalloc2(&d_buffer, buffer_size));
-    checkCudaErrors(cudaMalloc2((void**)&d_perm, num_e * sizeof(int)));
-    checkCudaErrors(cusparseCreateIdentityPermutation(handle, num_e, d_perm));
-    checkCudaErrors(cusparseXcoosortByColumn(handle, num_v, num_v, num_e, d_row_idx, d_col_idx, d_perm, d_buffer));
-    block_size = 128;
-    num_blocks = (num_e + block_size - 1) / block_size;
-    transform_values_kernel<<<num_blocks, block_size>>>(d_perm, d_val, d_value, num_e);
-    checkCudaErrors(cudaFree(d_buffer));
-    checkCudaErrors(cudaFree(d_perm));
-
-    // Decide grid and block size for spmm_kernel_opt
-    block.y = 32;
-    grid.x = (num_e + block.y - 1) / block.y;
-    block.x = 32;
-}
-
-void SpMMOpt::run(float *vin, float *vout)
-{
-    // TODO: your code
-    spmm_kernel_opt<<<grid, block>>>(d_col_idx, d_row_idx, d_value, vin, vout, num_v, num_e, feat_in);
+    else
+    {
+        block.y = 8;
+        grid.x = (num_v + block.y - 1) / block.y;
+        block.x = 128;
+        spmm_kernel_opt256<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v);
+    }
 }
---
+++ PA3/src/spmm_opt.old.cu
@@ -0,0 +1,84 @@
+#include "spmm_opt.h"
+#include "util.h"
+
+#define SMALL_WIDTH 32
+#define LARGE_WIDTH 256
+#define BLOCK_LEN 32
+
+__global__ void csr2coo_kernel(int *ptr, int *coo, int num_v)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid >= num_v) return;
+    int begin = ptr[tid], end = ptr[tid + 1];
+    for (int i = begin; i < end; ++i)
+    {
+        coo[i] = tid;
+    }
+}
+
+__global__ void transform_values_kernel(int *perm, float *val, float *new_val, int num_e)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid >= num_e) return;
+    new_val[tid] = val[perm[tid]];
+}
+
+__global__ void spmm_kernel_opt(int *col_idx, int *row_idx, float *value, float *vin, float *vout, int num_v, int num_e, int feat_in)
+{
+    int tid = blockIdx.x * blockDim.y + threadIdx.y;
+    if (tid >= num_e) return;
+    int row = row_idx[tid];
+    int col = col_idx[tid];
+    float val = value[tid];
+    int src_idx = col * feat_in + threadIdx.x;
+    int dst_idx = row * feat_in + threadIdx.x;
+    for (int j = 0; j < feat_in; j += blockDim.x)
+    {
+        atomicAdd(&vout[dst_idx + j], vin[src_idx + j] * val);
+    }
+}
+
+void SpMMOpt::preprocess(float *vin, float *vout)
+{
+    int block_size, num_blocks;
+    // Convert CSR to COO
+    block_size = 128;
+    num_blocks = (num_v + block_size - 1) / block_size;
+    checkCudaErrors(cudaMalloc2((void**)&d_coo, num_e * sizeof(int)));
+    csr2coo_kernel<<<num_blocks, block_size>>>(d_ptr, d_coo, num_v);
+
+    // Copy Data for transformation
+    checkCudaErrors(cudaMalloc2((void**)&d_col_idx, num_e * sizeof(int)));
+    checkCudaErrors(cudaMalloc2((void**)&d_row_idx, num_e * sizeof(int)));
+    checkCudaErrors(cudaMalloc2((void**)&d_value, num_e * sizeof(float)));
+    checkCudaErrors(cudaMemcpy(d_row_idx, d_coo, num_e * sizeof(int), cudaMemcpyDeviceToDevice));
+    checkCudaErrors(cudaMemcpy(d_col_idx, d_idx, num_e * sizeof(int), cudaMemcpyDeviceToDevice));
+    checkCudaErrors(cudaMemcpy(d_value, d_val, num_e * sizeof(float), cudaMemcpyDeviceToDevice));
+
+    // Convert Row-major to Column-major
+    void *d_buffer;
+    size_t buffer_size;
+    int *d_perm;
+    checkCudaErrors(cusparseCreate(&handle));
+    checkCudaErrors(cusparseXcoosort_bufferSizeExt(handle, num_v, num_v, num_e, d_row_idx, d_col_idx, &buffer_size));
+    checkCudaErrors(cudaMalloc2(&d_buffer, buffer_size));
+    checkCudaErrors(cudaMalloc2((void**)&d_perm, num_e * sizeof(int)));
+    checkCudaErrors(cusparseCreateIdentityPermutation(handle, num_e, d_perm));
+    checkCudaErrors(cusparseXcoosortByColumn(handle, num_v, num_v, num_e, d_row_idx, d_col_idx, d_perm, d_buffer));
+    block_size = 128;
+    num_blocks = (num_e + block_size - 1) / block_size;
+    transform_values_kernel<<<num_blocks, block_size>>>(d_perm, d_val, d_value, num_e);
+    checkCudaErrors(cudaFree(d_buffer));
+    checkCudaErrors(cudaFree(d_perm));
+
+    // Decide grid and block size for spmm_kernel_opt
+    block.y = 32;
+    grid.x = (num_e + block.y - 1) / block.y;
+    block.x = 32;
+}
+
+void SpMMOpt::run(float *vin, float *vout)
+{
+    // TODO: your code
+    spmm_kernel_opt<<<grid, block>>>(d_col_idx, d_row_idx, d_value, vin, vout, num_v, num_e, feat_in);
+}
