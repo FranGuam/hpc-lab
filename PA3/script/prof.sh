8bcc7505530d444da8b2d8fe1733eb391e35eecf
FranGuam <frankguansy@icloud.com>
2024-06-28 02:35:53 +0800

    fix(PA3)

--- PA3/script/prof.sh
+++ PA3/script/prof.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+source /home/spack/spack/share/spack/setup-env.sh
+
+spack load cuda
+
 dataset=$1
 len=$2
 
-srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --print-gpu-summary ./test/unit_tests --dataset $1 --len $2 --datadir ./data/
-srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --print-gpu-trace ./test/unit_tests --dataset $1 --len $2 --datadir ./data/
-srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --events shared_ld_bank_conflict,shared_st_bank_conflict ./test/unit_tests --dataset $1 --len $2 --datadir ./data/
-srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics achieved_occupancy,gld_throughput,gld_efficiency,ipc ./test/unit_tests --dataset $1 --len $2 --datadir ./data/
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --print-gpu-summary ./build/test/unit_tests --dataset $1 --len $2 --datadir ./data/
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --print-gpu-trace ./build/test/unit_tests --dataset $1 --len $2 --datadir ./data/
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --events shared_ld_bank_conflict,shared_st_bank_conflict ./build/test/unit_tests --dataset $1 --len $2 --datadir ./data/
+srun -N 1 --gres=gpu:1 nvprof --profile-from-start off --metrics achieved_occupancy,gld_throughput,gld_efficiency,ipc ./build/test/unit_tests --dataset $1 --len $2 --datadir ./data/
