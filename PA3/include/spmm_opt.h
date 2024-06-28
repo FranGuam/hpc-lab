15286d582ca7971100efeba1f2b8ee5ad7e93db6
FranGuam <frankguansy@icloud.com>
2024-06-28 22:56:19 +0800

    fix(PA3)

--- PA3/include/spmm_opt.h
+++ PA3/include/spmm_opt.h
@@ -1,29 +1,16 @@
 #ifndef SpMM_OPT_H
 #define SpMM_OPT_H
 #include "spmm_base.h"
-#include <cusparse.h>
 
 class SpMMOpt : public SpMM
 {
 public:
     SpMMOpt(int *dev_out_ptr, int *dev_out_idx, int out_num_v, int out_num_e, int out_feat_in) : SpMM(dev_out_ptr, dev_out_idx, out_num_v, out_num_e, out_feat_in) {}
     SpMMOpt(CSR *g, int out_feat_in) : SpMM(g, out_feat_in) {}
-    ~SpMMOpt() {
-        if (d_coo) checkCudaErrors(cudaFree(d_coo));
-        if (d_col_idx) checkCudaErrors(cudaFree(d_col_idx));
-        if (d_row_idx) checkCudaErrors(cudaFree(d_row_idx));
-        if (d_value) checkCudaErrors(cudaFree(d_value));
-    }
+    ~SpMMOpt() {}
      
     virtual void preprocess(float *vin, float *vout);
 
     virtual void run(float *vin, float *vout);
-
-private:
-    cusparseHandle_t handle = 0;
-    int *d_coo;
-    int *d_col_idx;
-    int *d_row_idx;
-    float *d_value;
 };
 #endif
--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -2,21 +2,21 @@
 #include "util.h"
 #include "cuda_profiler_api.h"
 
-#define WARP_SIZE 32
 #define ROW_NUM_32 1
+#define ROW_THREAD_32 32
 #define ROW_ELEM_32 64
 #define ROW_NUM_256 1
+#define ROW_THREAD_256 128
 #define ROW_ELEM_256 128
-#define CF_256 2
 
-__global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v)
+__global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, float *vout)
 {
     __shared__ int s_idx[ROW_NUM_32 * ROW_ELEM_32];
     __shared__ float s_val[ROW_NUM_32 * ROW_ELEM_32];
-    int tid = blockIdx.x * blockDim.y + threadIdx.y;
+    // int tid = blockIdx.x * blockDim.y + threadIdx.y;
     // if (tid >= num_v) return;
-    int begin = ptr[tid];
-    int len = ptr[tid + 1] - begin;
+    int begin = ptr[blockIdx.x];
+    int len = ptr[blockIdx.x + 1] - begin;
     // int offset = threadIdx.y * ROW_ELEM_32;
     // int *s_idx_base = s_idx + offset;
     // float *s_val_base = s_val + offset;
@@ -34,21 +34,20 @@ __global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, fl
         {
             *s_idx_base = *idx_base;
             *s_val_base = *val_base;
-            idx_base += WARP_SIZE;
-            val_base += WARP_SIZE;
-            if (i > threadIdx.x + WARP_SIZE)
+            idx_base += ROW_THREAD_32;
+            val_base += ROW_THREAD_32;
+            if (i > threadIdx.x + ROW_THREAD_32)
             {
-                s_idx_base[WARP_SIZE] = *idx_base;
-                s_val_base[WARP_SIZE] = *val_base;
-                idx_base += WARP_SIZE;
-                val_base += WARP_SIZE;
+                s_idx_base[ROW_THREAD_32] = *idx_base;
+                s_val_base[ROW_THREAD_32] = *val_base;
+                idx_base += ROW_THREAD_32;
+                val_base += ROW_THREAD_32;
             }
         }
         __syncwarp();
 
         // Compute
-        int max = min(ROW_ELEM_32, i);
-        for (int j = 0; j < max; ++j)
+        for (int j = 0; j < ROW_ELEM_32 && j < i; j++)
         {
             tmp += vin_base[(s_idx[j] << 5)] * s_val[j];
         }
@@ -57,20 +56,21 @@ __global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, fl
     vout[(tid << 5) + threadIdx.x] = tmp;
 }
 
-__global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v)
+__global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, float *vout)
 {
     __shared__ int s_idx[ROW_NUM_256 * ROW_ELEM_256];
     __shared__ float s_val[ROW_NUM_256 * ROW_ELEM_256];
-    int tid = blockIdx.x * blockDim.y + threadIdx.y;
+    // int tid = blockIdx.x * blockDim.y + threadIdx.y;
     // if (tid >= num_v) return;
-    int begin = ptr[tid], end = ptr[tid + 1];
+    // int begin = ptr[tid], end = ptr[tid + 1];
+    int begin = ptr[blockIdx.x], end = ptr[blockIdx.x + 1];
     // int offset = threadIdx.y * ROW_ELEM_256;
     // int *s_idx_base = s_idx + offset;
     // float *s_val_base = s_val + offset;
     int *s_idx_base = s_idx + threadIdx.x;
     float *s_val_base = s_val + threadIdx.x;
     float *vin_base1 = vin + threadIdx.x;
-    float *vin_base2 = vin + threadIdx.x + ROW_ELEM_256;
+    float *vin_base2 = vin + threadIdx.x + ROW_THREAD_256;
 
     float tmp1 = 0, tmp2 = 0;
     for (int i = begin; i < end; i += ROW_ELEM_256)
@@ -96,7 +96,7 @@ __global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, f
         __syncthreads();
     }
     vout[(tid << 8) + threadIdx.x] = tmp1;
-    vout[(tid << 8) + threadIdx.x + ROW_ELEM_256] = tmp2;
+    vout[(tid << 8) + threadIdx.x + ROW_THREAD_256] = tmp2;
 }
 
 void SpMMOpt::preprocess(float *vin, float *vout)
@@ -106,14 +106,14 @@ void SpMMOpt::preprocess(float *vin, float *vout)
         block.y = ROW_NUM_32;
         // grid.x = (num_v + block.y - 1) / block.y;
         grid.x = num_v;
-        block.x = ROW_ELEM_32;
+        block.x = ROW_THREAD_32;
     }
     else
     {
         block.y = ROW_NUM_256;
         // grid.x = (num_v + block.y - 1) / block.y;
         grid.x = num_v;
-        block.x = ROW_ELEM_256;
+        block.x = ROW_THREAD_256;
     }
 }
 
@@ -122,11 +122,11 @@ void SpMMOpt::run(float *vin, float *vout)
     checkCudaErrors(cudaProfilerStart());
     if (feat_in == 32)
     {
-        spmm_kernel_opt32<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v);
+        spmm_kernel_opt32<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout);
     }
     else
     {
-        spmm_kernel_opt256<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v);
+        spmm_kernel_opt256<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout);
     }
     checkCudaErrors(cudaProfilerStop());
 }
