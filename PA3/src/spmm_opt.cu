7050cabd3330931c73a7f92fda138c89538e9b1d
FranGuam <frankguansy@icloud.com>
2024-06-30 00:28:23 +0800

    refactor(PA3)

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -2,26 +2,20 @@
 #include "util.h"
 #include <vector>
 
-#define ROW_NUM_32 1
 #define ROW_THREAD_32 32
 #define ROW_ELEM_32 64
-#define ROW_NUM_256 1
 #define ROW_THREAD_256 128
 #define ROW_ELEM_256 128
 
 __global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, float *vout, int *iperm, bool use_perm)
 {
-    __shared__ int s_idx[ROW_NUM_32 * ROW_ELEM_32];
-    __shared__ float s_val[ROW_NUM_32 * ROW_ELEM_32];
-    // int tid = blockIdx.x * blockDim.y + threadIdx.y;
-    // if (tid >= num_v) return;
+    __shared__ int s_idx[ROW_ELEM_32];
+    __shared__ float s_val[ROW_ELEM_32];
     int begin = ptr[blockIdx.x];
     int end = ptr[blockIdx.x + 1];
     int bound = end - threadIdx.x;
     int dest = use_perm ? iperm[blockIdx.x] : blockIdx.x;
-    // int offset = threadIdx.y * ROW_ELEM_32;
-    // int *s_idx_base = s_idx + offset;
-    // float *s_val_base = s_val + offset;
+
     int *s_idx_base = s_idx + threadIdx.x;
     float *s_val_base = s_val + threadIdx.x;
     float *vin_base = vin + threadIdx.x;
@@ -53,24 +47,18 @@ __global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, fl
         }
         __syncwarp();
     }
-    // vout[(tid << 5) + threadIdx.x] = tmp;
     vout[(dest << 5) + threadIdx.x] = tmp;
 }
 
 __global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, float *vout, int *iperm, bool use_perm)
 {
-    __shared__ int s_idx[ROW_NUM_256 * ROW_ELEM_256];
-    __shared__ float s_val[ROW_NUM_256 * ROW_ELEM_256];
-    // int tid = blockIdx.x * blockDim.y + threadIdx.y;
-    // if (tid >= num_v) return;
-    // int begin = ptr[tid], end = ptr[tid + 1];
+    __shared__ int s_idx[ROW_ELEM_256];
+    __shared__ float s_val[ROW_ELEM_256];
     int begin = ptr[blockIdx.x];
     int end = ptr[blockIdx.x + 1];
     int bound = end - threadIdx.x;
     int dest = use_perm ? iperm[blockIdx.x] : blockIdx.x;
-    // int offset = threadIdx.y * ROW_ELEM_256;
-    // int *s_idx_base = s_idx + offset;
-    // float *s_val_base = s_val + offset;
+
     int *s_idx_base = s_idx + threadIdx.x;
     float *s_val_base = s_val + threadIdx.x;
     float *vin_base1 = vin + threadIdx.x;
@@ -100,8 +88,6 @@ __global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, f
         }
         __syncthreads();
     }
-    // vout[(tid << 8) + threadIdx.x] = tmp1;
-    // vout[(tid << 8) + threadIdx.x + ROW_THREAD_256] = tmp2;
     vout += (dest << 8) + threadIdx.x;
     *vout = tmp1;
     vout[ROW_THREAD_256] = tmp2;
@@ -109,7 +95,43 @@ __global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, f
 
 void SpMMOpt::preprocess(float *vin, float *vout)
 {
-    int zero_rows = 0;
+    // Set grid and block size
+    grid.x = num_v;
+    if (feat_in == 32) block.x = ROW_THREAD_32;
+    else block.x = ROW_THREAD_256;
+
+    if (feat_in == 32)
+    {
+        // if (num_v == 169343) use_perm = false;
+        // if (num_v == 132534) use_perm = false;
+        // if (num_v == 1138499) use_perm = false;
+        // if (num_v == 2500604) use_perm = false;
+        // if (num_v == 881680) use_perm = false;
+    }
+    else
+    {
+        // if (num_v == 132534) use_perm = false;
+        // if (num_v == 2500604) use_perm = false;
+        // if (num_v == 881680) use_perm = false;
+    }
+
+    if (!use_perm) return;
+
+    bool use_perm_col = true;
+    if (feat_in == 32)
+    {
+        // if (num_v == 169343) use_perm_col = false;
+        // if (num_v == 132534) use_perm_col = false;
+        // if (num_v == 1138499) use_perm_col = false;
+        // if (num_v == 2500604) use_perm_col = false;
+        // if (num_v == 881680) use_perm_col = false;
+    }
+    else
+    {
+        // if (num_v == 132534) use_perm_col = false;
+        // if (num_v == 2500604) use_perm_col = false;
+        // if (num_v == 881680) use_perm_col = false;
+    }
 
     // Copy data to host
     std::vector<int> row_ptr(num_v + 1);
@@ -120,28 +142,31 @@ void SpMMOpt::preprocess(float *vin, float *vout)
     checkCudaErrors(cudaMemcpy(val.data(), d_val, sizeof(float) * num_e, cudaMemcpyDeviceToHost));
     // Re-order the columns of the matrix
     std::vector<std::vector<int>> row_columns(num_v);
-    for (int row = 0; row < num_v; ++row) {
-        for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; ++idx) {
-            row_columns[row].push_back(col_idx[idx]);
-        }
-    }
     std::vector<int> perm_col(num_v, -1);
     std::vector<int> iperm_col(num_v, -1);
-    int new_col = 0;
-    for (int row = 0; row < num_v; ++row) {
-        for (int col : row_columns[row]) {
-            if (perm_col[col] == -1) {
-                perm_col[col] = new_col++;
-                iperm_col[perm_col[col]] = col;
+    if (use_perm_col) {
+        for (int row = 0; row < num_v; ++row) {
+            for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; ++idx) {
+                row_columns[row].push_back(col_idx[idx]);
+            }
+        }
+        int new_col = 0;
+        for (int row = 0; row < num_v; ++row) {
+            for (int col : row_columns[row]) {
+                if (perm_col[col] == -1) {
+                    perm_col[col] = new_col++;
+                    iperm_col[perm_col[col]] = col;
+                }
+            }
+        }
+        for (int row = 0; row < num_v; ++row) {
+            for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; ++idx) {
+                col_idx[idx] = perm_col[col_idx[idx]];
             }
         }
     }
-    for (int row = 0; row < num_v; ++row) {
-        for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; ++idx) {
-            col_idx[idx] = perm_col[col_idx[idx]];
-        }
-    }
     // Re-order the rows of the matrix
+    int zero_rows = 0;
     std::vector<std::vector<int>> column_rows(num_v);
     for (int row = 0; row < num_v; ++row) {
         if (row_ptr[row] == row_ptr[row + 1]) {
@@ -152,6 +177,7 @@ void SpMMOpt::preprocess(float *vin, float *vout)
             column_rows[col_idx[idx]].push_back(row);
         }
     }
+    grid.x = num_v - zero_rows;
     std::vector<int> perm(num_v, -1);
     std::vector<int> iperm(num_v, -1);
     int new_row = 0;
@@ -191,42 +217,19 @@ void SpMMOpt::preprocess(float *vin, float *vout)
         }
     }
     // Recover column permutation
-    for (int new_row = 0; new_row < num_v; ++new_row) {
-        for (int new_idx = new_row_ptr[new_row]; new_idx < new_row_ptr[new_row + 1]; ++new_idx) {
-            new_col_idx[new_idx] = iperm_col[new_col_idx[new_idx]];
+    if (use_perm_col) {
+        for (int new_row = 0; new_row < num_v; ++new_row) {
+            for (int new_idx = new_row_ptr[new_row]; new_idx < new_row_ptr[new_row + 1]; ++new_idx) {
+                new_col_idx[new_idx] = iperm_col[new_col_idx[new_idx]];
+            }
         }
     }
-    // Set grid and block size
-    if (feat_in == 32)
-    {
-        // if (num_v == 169343) use_perm = false;
-        // if (num_v == 132534) use_perm = false;
-        // if (num_v == 1138499) use_perm = false;
-        // if (num_v == 2500604) use_perm = false;
-        // if (num_v == 881680) use_perm = false;
-        block.y = ROW_NUM_32;
-        // grid.x = (num_v + block.y - 1) / block.y;
-        grid.x = use_perm ? (num_v - zero_rows) : num_v;
-        block.x = ROW_THREAD_32;
-    }
-    else
-    {
-        // if (num_v == 132534) use_perm = false;
-        // if (num_v == 2500604) use_perm = false;
-        // if (num_v == 881680) use_perm = false;
-        block.y = ROW_NUM_256;
-        // grid.x = (num_v + block.y - 1) / block.y;
-        grid.x = use_perm ? (num_v - zero_rows) : num_v;
-        block.x = ROW_THREAD_256;
-    }
     // Copy data back to device
-    if (use_perm) {
-        checkCudaErrors(cudaMemcpy(d_ptr, new_row_ptr.data(), sizeof(int) * (num_v + 1), cudaMemcpyHostToDevice));
-        checkCudaErrors(cudaMemcpy(d_idx, new_col_idx.data(), sizeof(int) * num_e, cudaMemcpyHostToDevice));
-        checkCudaErrors(cudaMemcpy(d_val, new_val.data(), sizeof(float) * num_e, cudaMemcpyHostToDevice));
-        checkCudaErrors(cudaMalloc2((void**)&d_iperm, sizeof(int) * num_v));
-        checkCudaErrors(cudaMemcpy(d_iperm, iperm.data(), sizeof(int) * num_v, cudaMemcpyHostToDevice));
-    }
+    checkCudaErrors(cudaMemcpy(d_ptr, new_row_ptr.data(), sizeof(int) * (num_v + 1), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_idx, new_col_idx.data(), sizeof(int) * num_e, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_val, new_val.data(), sizeof(float) * num_e, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMalloc2((void**)&d_iperm, sizeof(int) * num_v));
+    checkCudaErrors(cudaMemcpy(d_iperm, iperm.data(), sizeof(int) * num_v, cudaMemcpyHostToDevice));
 }
 
 void SpMMOpt::run(float *vin, float *vout)
