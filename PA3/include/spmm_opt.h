9db1633e8d29931626987c1ddc461a0ad5863631
FranGuam <frankguansy@icloud.com>
2024-06-29 18:40:17 +0800

    feat(PA3): preprocess

--- PA3/include/spmm_opt.h
+++ PA3/include/spmm_opt.h
@@ -7,10 +7,16 @@ class SpMMOpt : public SpMM
 public:
     SpMMOpt(int *dev_out_ptr, int *dev_out_idx, int out_num_v, int out_num_e, int out_feat_in) : SpMM(dev_out_ptr, dev_out_idx, out_num_v, out_num_e, out_feat_in) {}
     SpMMOpt(CSR *g, int out_feat_in) : SpMM(g, out_feat_in) {}
-    ~SpMMOpt() {}
+    ~SpMMOpt() {
+        if (d_iperm) checkCudaErrors(cudaFree(d_iperm));
+    }
      
     virtual void preprocess(float *vin, float *vout);
 
     virtual void run(float *vin, float *vout);
+
+private:
+
+    int *d_iperm;
 };
 #endif
--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -1,4 +1,6 @@
 #include "spmm_opt.h"
+#include "util.h"
+#include <vector>
 
 #define ROW_NUM_32 1
 #define ROW_THREAD_32 32
@@ -7,7 +9,7 @@
 #define ROW_THREAD_256 128
 #define ROW_ELEM_256 128
 
-__global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, float *vout)
+__global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, float *vout, int *iperm)
 {
     __shared__ int s_idx[ROW_NUM_32 * ROW_ELEM_32];
     __shared__ float s_val[ROW_NUM_32 * ROW_ELEM_32];
@@ -44,17 +46,17 @@ __global__ void spmm_kernel_opt32(int *ptr, int *idx, float *val, float *vin, fl
 
         // Compute
         int max = min(ROW_ELEM_32, end - i);
-        for (int j = 0; j < max; j++)
+        for (int j = 0; j < max; ++j)
         {
             tmp += vin_base[(s_idx[j] << 5)] * s_val[j];
         }
         __syncwarp();
     }
     // vout[(tid << 5) + threadIdx.x] = tmp;
-    vout[(blockIdx.x << 5) + threadIdx.x] = tmp;
+    vout[(iperm[blockIdx.x] << 5) + threadIdx.x] = tmp;
 }
 
-__global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, float *vout)
+__global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, float *vout, int *iperm)
 {
     __shared__ int s_idx[ROW_NUM_256 * ROW_ELEM_256];
     __shared__ float s_val[ROW_NUM_256 * ROW_ELEM_256];
@@ -98,7 +100,7 @@ __global__ void spmm_kernel_opt256(int *ptr, int *idx, float *val, float *vin, f
     }
     // vout[(tid << 8) + threadIdx.x] = tmp1;
     // vout[(tid << 8) + threadIdx.x + ROW_THREAD_256] = tmp2;
-    vout += (blockIdx.x << 8) + threadIdx.x;
+    vout += (iperm[blockIdx.x] << 8) + threadIdx.x;
     *vout = tmp1;
     vout[ROW_THREAD_256] = tmp2;
 }
@@ -119,16 +121,69 @@ void SpMMOpt::preprocess(float *vin, float *vout)
         grid.x = num_v;
         block.x = ROW_THREAD_256;
     }
+
+    // Copy data to host
+    std::vector<int> row_ptr(num_v + 1);
+    std::vector<int> col_idx(num_e);
+    std::vector<float> val(num_e);
+    checkCudaErrors(cudaMemcpy(row_ptr.data(), d_ptr, sizeof(int) * (num_v + 1), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(col_idx.data(), d_idx, sizeof(int) * num_e, cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(val.data(), d_val, sizeof(float) * num_e, cudaMemcpyDeviceToHost));
+    // Re-order the rows of the matrix
+    std::vector<std::vector<int>> columns(num_v);
+    for (int row = 0; row < num_v; ++row) {
+        for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; ++idx) {
+            columns[col_idx[idx]].push_back(row);
+        }
+    }
+    std::vector<int> perm(num_v, -1);
+    std::vector<int> iperm(num_v, -1);
+    int new_row = 0;
+    for (int col = 0; col < num_v; ++col) {
+        for (int row : columns[col]) {
+            if (perm[row] == -1) {
+                perm[row] = new_row++;
+                iperm[perm[row]] = row;
+            }
+        }
+    }
+    // Create a new matrix
+    std::vector<int> new_row_ptr(num_v + 1);
+    std::vector<int> new_col_idx(num_e);
+    std::vector<float> new_val(num_e);
+    std::vector<int> new_row_len(num_v);
+    for (int row = 0; row < num_v; ++row) {
+        int new_row = perm[row];
+        new_row_len[new_row] = row_ptr[row + 1] - row_ptr[row];
+    }
+    new_row_ptr[0] = 0;
+    for (int new_row = 0; new_row < num_v; ++new_row) {
+        new_row_ptr[new_row + 1] = new_row_ptr[new_row] + new_row_len[new_row];
+    }
+    for (int row = 0; row < matrix.num_rows; ++row) {
+        int new_row = perm[row];
+        int new_idx = new_row_ptr[new_row];
+        for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; ++idx, ++new_idx) {
+            new_col_idx[new_idx] = col_idx[idx];
+            new_val[new_idx] = val[idx];
+        }
+    }
+    // Copy data back to device
+    checkCudaErrors(cudaMemcpy(d_ptr, new_row_ptr.data(), sizeof(int) * (num_v + 1), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_idx, new_col_idx.data(), sizeof(int) * num_e, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_val, new_val.data(), sizeof(float) * num_e, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMalloc2(&d_iperm, sizeof(int) * num_v));
+    checkCudaErrors(cudaMemcpy(d_iperm, iperm.data(), sizeof(int) * num_v, cudaMemcpyHostToDevice));
 }
 
 void SpMMOpt::run(float *vin, float *vout)
 {
     if (feat_in == 32)
     {
-        spmm_kernel_opt32<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout);
+        spmm_kernel_opt32<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, d_iperm);
     }
     else
     {
-        spmm_kernel_opt256<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout);
+        spmm_kernel_opt256<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, d_iperm);
     }
 }
